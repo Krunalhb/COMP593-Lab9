6be044f4fef09c939857450ca3de76d2cb3564f8
Krunalhb <krunalhimanshu.brahmbhatt@flemingcollege.ca>
2023-04-03 18:53:09 -0400

    made necessary changes to the script
    defined main function

--- poke_api.py
+++ poke_api.py
@@ -2,6 +2,15 @@ import requests
 
 POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/"
 
+def main():
+    pokemon_name = input("Enter the name of a Pokémon to fetch its information: ")
+    pokemon_info = get_pokemon_info(pokemon_name)
+    if pokemon_info:
+        print("Here is the information for " + pokemon_name.title() + ":")
+        print(pokemon_info)
+    else:
+        print("Failed to retrieve information for " + pokemon_name.title() + ". Please try again.")
+
 def get_pokemon_info(pokemon_name):
     """
     Gets information about a specified Pokémon from the PokéAPI.
@@ -13,7 +22,7 @@ def get_pokemon_info(pokemon_name):
         dict: A dictionary containing all the Pokémon information fetched from the PokéAPI, if retrieved
         successfully. Returns None if the Pokémon information is not fetched successfully.
     """
-    pokemon_name = pokemon_name.strip().lower()
+    pokemon_name = str(pokemon_name).strip().lower()
 
     # Construct the API URL for the specified Pokémon
     url = POKE_API_URL + pokemon_name
@@ -25,17 +34,12 @@ def get_pokemon_info(pokemon_name):
     # Check if the request was successful
     if resp_msg.status_code == requests.codes.ok:
         print('success')
-        # Return dictionary of pokemon info
+        #Return dictionary of pokemon info
         return resp_msg.json()
     else:
         print('failure')
         print(f'Response code: {resp_msg.status_code} ({resp_msg.content})')
-        return None
+        return
 
-if __name__ == '__main__':
-    pokemon_name = input('Enter a Pokemon name: ')
-    pokemon_info = get_pokemon_info(pokemon_name)
-    if pokemon_info:
-        print(pokemon_info)
-    else:
-        print(f"Couldn't fetch information for {pokemon_name}.")
+if __name__=='__main__':
+    main()
